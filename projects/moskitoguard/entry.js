67194e7edbfc1270a94d8db11df6578c0ccab61f
Souler <soulerhyd@gmail.com>
2016-03-28 00:24:22 +0200

    Update parallax in bundle

--- projects/moskitoguard/entry.js
+++ projects/moskitoguard/entry.js
@@ -1,5 +1,6 @@
 require('../../css/jquery.modal.css')
 require('../../js/jquery.modal.js')
+require('../../js/jquery.parallax.js')
 require('../../js/jquery.divi-nav-section.js')
-jQuery(function($){$('.paralax').parallax()});
+jQuery(function($){$('.parallax').parallax()});
 jQuery(function($){$('a[href^="#modal"]').leanModal();});
