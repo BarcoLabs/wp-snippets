68f012a740c5a36497c769b1a0dc254cad6f67c8
Souler <soulerhyd@gmail.com>
2016-03-28 00:20:26 +0200

    Rename parallax selector

--- projects/moskitoguard/bundle.js
+++ projects/moskitoguard/bundle.js
@@ -47,6 +47,8 @@
 	__webpack_require__(1)
 	__webpack_require__(5)
 	__webpack_require__(6)
+	jQuery(function($){$('.paralax').parallax()});
+	jQuery(function($){$('a[href^="#modal"]').leanModal();});
 
 /***/ },
 /* 1 */
@@ -405,7 +407,6 @@
 
 	(function($){$.fn.extend({leanModal:function(options){var defaults={top:100,overlay:0.5,closeButton:null};var overlay=$("<div id='lean_overlay'></div>");$("body").append(overlay);options=$.extend(defaults,options);return this.each(function(){var o=options;$(this).click(function(e){var modal_id=$(this).attr("href");$("#lean_overlay").click(function(){close_modal(modal_id)});$(o.closeButton).click(function(){close_modal(modal_id)});var modal_height=$(modal_id).outerHeight();var modal_width=$(modal_id).outerWidth();
 	$("#lean_overlay").css({"display":"block",opacity:0});$("#lean_overlay").fadeTo(200,o.overlay);$(modal_id).css({"display":"block","position":"fixed","opacity":0,"z-index":11000,"left":50+"%","margin-left":-(modal_width/2)+"px","top":o.top+"px"});$(modal_id).fadeTo(200,1);e.preventDefault()})});function close_modal(modal_id){$("#lean_overlay").fadeOut(200);$(modal_id).css({"display":"none"})}}})})(jQuery);
-	jQuery(function($){$("a[rel*=leanModal]").leanModal();});
 
 /***/ },
 /* 6 */
@@ -478,7 +479,11 @@
 					$menuContainer.find('a[href="' + hash + '"]').addClass('active')
 				}
 			})
-			$(window).trigger('hashchange')
+
+			if (window.location.hash && window.location.hash.length > 0)
+				$(window).trigger('hashchange')
+			else
+				$sections.first().show();
 		})
 	})()
 
--- projects/moskitoguard/entry.js
+++ projects/moskitoguard/entry.js
@@ -1,5 +1,5 @@
 require('../../css/jquery.modal.css')
 require('../../js/jquery.modal.js')
 require('../../js/jquery.divi-nav-section.js')
-jQuery(function($){$('#scene').parallax()});
+jQuery(function($){$('.paralax').parallax()});
 jQuery(function($){$('a[href^="#modal"]').leanModal();});
