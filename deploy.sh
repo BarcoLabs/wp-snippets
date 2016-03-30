b07379c605b9a970e2e65f99598ca5df5bae4edf
Juan José <soulerhyd@gmail.com>
2016-03-31 00:50:54 +0200

    Update TravisCI email on automatic builds

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,15 @@ rm -rf projects/*/bundles.js || exit 0;
 
 BUILD=$(./compile.sh)
 
+echo 
+echo $BUILD
+echo
+
 cd projects
 git init
 
-git config user.name "Travis CI"
-git config user.email "travis@barcolabs.com"
+git config user.name "travis-ci"
+git config user.email "contact@travis-ci.com"
 
 git add ./*/bundle.js
 git commit -m "Generate Webpack bundles" -m "" -m "${BUILD}"
