5335a22588fbb21235184087b2d8e0d18ab1dea5
Juan José <soulerhyd@gmail.com>
2016-04-13 23:52:29 +0200

    Add a wrapper around the the breadcrum

--- projects/tankat/entry.js
+++ projects/tankat/entry.js
@@ -1,5 +1,8 @@
 jQuery(function($) {
 	$('.breadcrumb')
 		.addClass('et_pb_row')
-		.insertAfter('.et_pb_section_0')
+		.appendTo(
+			$('<div>', { class: 'breadcrumb-container' })
+			.insertAfter('.et_pb_section_0')
+		)
 })
