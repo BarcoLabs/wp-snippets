991a043a708166583c4dcf2d9fde11c9454656f7
javimbk <javimbk@gmail.com>
2016-04-27 17:26:59 +0200

    moskito changes.

--- projects/moskitoguard/entry.js
+++ projects/moskitoguard/entry.js
@@ -11,7 +11,7 @@ jQuery(function($) {
 	$('body, a').css('cursor', cursor)
 	$('.parallax').parallax()
 	$('.filter-table input[type="submit"]').remove()
-	$('.filter-table input').filterTable('.drugstore-table', '.drugstore-no-result')
+	$('.filter-table input').filterTable('.drugstore-table', 'td:nth-child(1), td:nth-child(4)', '.drugstore-no-result')
 	$('a[href^="#modal"]').leanModal()
 	$('#main-footer').secondaryFooter('Utilice los biocidas de forma segura. Lea siempre la etiqueta y la información sobre el producto antes de usarlo')
 	$('#menu-footer.menu').addClass('bottom-nav')
