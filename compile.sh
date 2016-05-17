0a0006fce47e30e35198a188c3e5341bfd7b95b0
javimbk <javimbk@gmail.com>
2016-05-18 00:56:18 +0200

    add infobuilding project

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
 node_modules/.bin/webpack --config "./projects/moskitoguard/webpack.config.js" --color=false
 node_modules/.bin/webpack --config "./projects/tankat/webpack.config.js" --color=false
+node_modules/.bin/webpack --config "./projects/infobuilding/webpack.config.js" --color=false
+
---
+++ projects/infobuilding/entry.js
@@ -0,0 +1,4 @@
+require('../../js/jquery.divi.privacy-check')
+jQuery(function($) {
+  $('form.et_pb_contact_form').privacyCheck()
+})
