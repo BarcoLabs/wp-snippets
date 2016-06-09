a7be468b1442b149d2b269f6f14cf2cf9f5b29cf
Juan José <soulerhyd@gmail.com>
2016-06-09 20:11:50 +0200

    Add jornadasprl project

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@ node_modules/.bin/webpack --config "./projects/moskitoguard/webpack.config.js" -
 node_modules/.bin/webpack --config "./projects/tankat/webpack.config.js" --color=false
 node_modules/.bin/webpack --config "./projects/infobuilding/webpack.config.js" --color=false
 node_modules/.bin/webpack --config "./projects/bioservicios/webpack.config.js" --color=false
+node_modules/.bin/webpack --config "./projects/jornadasprl/webpack.config.js" --color=false
 
---
+++ projects/jornadasprl/entry.js
@@ -0,0 +1,5 @@
+require('./style.css')
+var logo_mess = require('./logo-mess.html')
+jQuery(function($) {
+  $('.logo_container').append(logo_mess)
+})
