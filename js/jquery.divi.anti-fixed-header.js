6edc726c7b7a685942e38b746e8f6e9b12581251
Juan José <soulerhyd@gmail.com>
2016-06-09 20:24:50 +0200

    Add jQuery Divi anti fixed header

---
+++ js/jquery.divi.anti-fixed-header.js
@@ -0,0 +1,13 @@
+// https://divibooster.com/stop-the-divi-header-from-shrinking-on-scroll/
+(function(){
+    // Override the addClass to prevent fixed header class from being added
+    var addclass = jQuery.fn.addClass;
+    jQuery.fn.addClass = function(){
+        var result = addclass.apply(this, arguments);
+            jQuery('#main-header').removeClass('et-fixed-header');
+        return result;
+    }
+})();
+jQuery(function($){
+    $('#main-header').removeClass('et-fixed-header');
+});
--- projects/jornadasprl/entry.js
+++ projects/jornadasprl/entry.js
@@ -1,5 +1,6 @@
 require('./style.css')
 var logo_mess = require('./logo-mess.html')
+require('../../js/jquery.divi.anti-fixed-header')
 jQuery(function($) {
   $('.logo_container').append(logo_mess)
 })
