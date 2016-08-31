c0e9a652e1db5a67d2644b8494a4a53f93502b88
Javier Ruiz Corisco <javimbk@gmail.com>
2016-08-31 12:23:48 +0200

    aquasportsvera added.

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@ node_modules/.bin/webpack --config "./projects/tankat/webpack.config.js" --color
 node_modules/.bin/webpack --config "./projects/infobuilding/webpack.config.js" --color=false
 node_modules/.bin/webpack --config "./projects/bioservicios/webpack.config.js" --color=false
 node_modules/.bin/webpack --config "./projects/jornadasprl/webpack.config.js" --color=false
-
+node_modules/.bin/webpack --config "./projects/aquasportsvera/webpack.config.js" --color=false
