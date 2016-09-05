0f9239f20cec60a30be9733a95ae9089815cba3f
Javier Ruiz Corisco <javimbk@gmail.com>
2016-09-06 00:36:20 +0200

    next: stupid try.

--- projects/nextstation2017/entry.js
+++ projects/nextstation2017/entry.js
@@ -1,1 +1,5 @@
 require('./style.css')
+jQuery(function($) {
+  $('#venues > div.col-md-3.col-sm-12.column').addClass('col-md-7')
+  $('#venues > div.col-md-3.col-md-7.col-sm-12.column').removeClass('col-md-3')
+})
