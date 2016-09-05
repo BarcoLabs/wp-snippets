5faf060a08d8742e99b5983a9bdf38649edb4eea
Javier Ruiz Corisco <javimbk@gmail.com>
2016-09-06 01:19:55 +0200

    next: video

--- projects/nextstation2017/entry.js
+++ projects/nextstation2017/entry.js
@@ -7,4 +7,13 @@ jQuery(function($) {
   // Switch Sponsors & Faq's position.
   var faqDiv = $('#faq').detach();
   $('#partners').append(faqDiv);
+
+  // Append Video, avoid iframe.
+  var video = $('<video />', {
+    id: 'video',
+    src: 'http://www.alcandora.com/proyectos/nextstation/nextstation.mp4',
+    type: 'video/mp4',
+    controls: true
+    });
+    video.appendTo($('#promo'))
 })
