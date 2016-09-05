2893dc964a8ad970b569b8d5b4906fe37347050f
Javier Ruiz Corisco <javimbk@gmail.com>
2016-09-06 00:52:59 +0200

    next: switch it up

--- projects/nextstation2017/entry.js
+++ projects/nextstation2017/entry.js
@@ -3,4 +3,9 @@ jQuery(function($) {
   $('#venues > div.col-md-3.col-sm-12.column').addClass('col-md-7')
   $('#venues > div.col-md-3.col-md-7.col-sm-12.column').removeClass('col-md-3')
   $('#directionsPanel').text("Placeholder for directions.")
+
+  // Switch Sponsors & Faq's position
+  var sponsorsDiv = ($('#partners')).detach();
+  ($('#faq')).append(div2);
+  
 })
