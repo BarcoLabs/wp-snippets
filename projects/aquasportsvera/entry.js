d4d89f42068c6ba1ac680db060f8283c7bba8399
Javier Ruiz Corisco <javimbk@gmail.com>
2016-09-12 00:32:31 +0200

    aqvera: faqs

--- projects/aquasportsvera/entry.js
+++ projects/aquasportsvera/entry.js
@@ -2,4 +2,6 @@ require('./style.css')
 jQuery(function($) {
   $('#aqvera-full-info-section > div > div').addClass('col-12')
   $('#aqvera-full-info-section > div > div').removeClass('col-6')
+  $('#faq > div > div').addClass('col-12')
+  $('#faq > div > div').removeClass('col-6')
 })
