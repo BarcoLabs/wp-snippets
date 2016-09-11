cc371bc83a7b950db3265ccca86433519b704a86
Juan José <soulerhyd@gmail.com>
2016-09-12 00:58:21 +0200

    add popup contact form to aquasports

--- projects/aquasportsvera/entry.js
+++ projects/aquasportsvera/entry.js
@@ -1,7 +1,9 @@
+require('../../js/jquery.popup-contactform.js')
 require('./style.css')
 jQuery(function($) {
   $('#aqvera-full-info-section > div > div').addClass('col-12')
   $('#aqvera-full-info-section > div > div').removeClass('col-6')
   $('#faq > div > div').addClass('col-12')
   $('#faq > div > div').removeClass('col-6')
+  $('slider-slide-actions a').popupContactform()
 })
