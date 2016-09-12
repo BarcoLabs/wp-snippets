cc465e0a921c26242bea32c81a2ab9affeba1b69
Javier Ruiz Corisco <javimbk@gmail.com>
2016-09-12 13:07:23 +0200

    aqvera: cta button yet again.

--- projects/aquasportsvera/entry.js
+++ projects/aquasportsvera/entry.js
@@ -6,7 +6,7 @@ jQuery(function($) {
   $('#faq > div > div').addClass('col-12')
   $('#faq > div > div').removeClass('col-6')
   $('.slider-slide-actions a').popupContactform()
-  $('#slider > div > div > div:nth-child(n) > div > div.slider-slide-main > div > div > a').removeClass('transparent')
   $('#slider > div > div > div:nth-child(n) > div > div.slider-slide-main > div > div > a').addClass('secondary')
+  $('#slider > div > div > div:nth-child(n) > div > div.slider-slide-main > div > div > a').removeClass('transparent')
   $('#actividades-dirigidas > div > div.col.col-6.col-last.home-info-section-1-content.in-viewport > a').attr('target', '_BLANK')
 })
