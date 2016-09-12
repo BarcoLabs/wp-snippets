d40fea7b0bd01cd802ef0c76a46078fd41feb1dc
Javier Ruiz Corisco <javimbk@gmail.com>
2016-09-12 11:32:23 +0200

    .

--- projects/aquasportsvera/entry.js
+++ projects/aquasportsvera/entry.js
@@ -6,5 +6,7 @@ jQuery(function($) {
   $('#faq > div > div').addClass('col-12')
   $('#faq > div > div').removeClass('col-6')
   $('.slider-slide-actions a').popupContactform()
-  setTimeout($('#actividades-dirigidas > div > div.col.col-6.col-last.home-info-section-1-content.in-viewport > a').attr('target', '_BLANK'), 2000);
+  setTimeout(function() {
+    $('#actividades-dirigidas > div > div.col.col-6.col-last.home-info-section-1-content.in-viewport > a').attr('target', '_BLANK');
+  }, 2000);
 })
