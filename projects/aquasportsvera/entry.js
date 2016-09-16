15d3a46fb455c50805c799095e01691d499ed049
Javier Ruiz Corisco <javimbk@gmail.com>
2016-09-16 12:23:48 +0200

    aqvera: replace text with img

--- projects/aquasportsvera/entry.js
+++ projects/aquasportsvera/entry.js
@@ -9,4 +9,5 @@ jQuery(function($) {
   $('#slider > div > div > div:nth-child(n) > div > div.slider-slide-main > div > div > a').delay(1000).addClass('secondary')
   $('#slider > div > div > div:nth-child(n) > div > div.slider-slide-main > div > div > a').removeClass('transparent')
   $('#actividades-dirigidas > div > div.col.col-6.col-last.home-info-section-1-content.in-viewport > a').attr('target', '_BLANK')
+  $('#slider > div > div > div:nth-child(1) > div > div.slider-slide-main > div > h4').replaceWith('<img src="http://aquasportsvera-es.alcandora.com/wp-content/uploads/2016/09/piscinagym.png">')
 })
