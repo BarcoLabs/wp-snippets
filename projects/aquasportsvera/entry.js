28b0cb324c226f5d3bb930b3130b914866d25e00
Javier Ruiz Corisco <javimbk@gmail.com>
2016-09-16 12:42:35 +0200

    aqvera: cta button

--- projects/aquasportsvera/entry.js
+++ projects/aquasportsvera/entry.js
@@ -6,7 +6,7 @@ jQuery(function($) {
   $('#faq > div > div').addClass('col-12')
   $('#faq > div > div').removeClass('col-6')
   $('.slider-slide-actions a').popupContactform()
-  $('.slider-slide-actions a').addClass('secondary')
+  $('.slider-slide-actions a').addClass('secondary cta-button')
   $('.slider-slide-actions a').removeClass('transparent')
   $('#actividades-dirigidas > div > div.col.col-6.col-last.home-info-section-1-content.in-viewport > a').attr('target', '_BLANK')
   $("h4:contains('img-cta')").html('<img src="http://aquasportsvera-es.alcandora.com/wp-content/uploads/2016/09/piscinagym.png"/>')
