9717b0ed329e6dd316b866c2c8c4aaa2b7e59fce
Juan José <soulerhyd@gmail.com>
2016-09-20 02:52:04 +0200

    Rework contact form logic

--- projects/aquasportsvera/entry.js
+++ projects/aquasportsvera/entry.js
@@ -1,11 +1,15 @@
-require('../../js/jquery.popup-contactform.js')
 require('./style.css')
 jQuery(function($) {
   $('#aqvera-full-info-section > div > div').addClass('col-12')
   $('#aqvera-full-info-section > div > div').removeClass('col-6')
   $('#faq > div > div').addClass('col-12')
   $('#faq > div > div').removeClass('col-6')
-  $('.slider-slide-actions a').popupContactform()
+  // Subscription popup stuff
+  var $popupSub = $('#popup-subscription.home-custom')
+  $popupSub.click(function() { $popupSub.hide() })
+  $popupSub.find('> * ').click(function(e) { e.stopPropagation() })
+  $('.slider-slide-actions a').click(function() { $popupSub.toggle() })
+  // -- Subscription popup stuff
   $('.slider-slide-actions a').addClass('secondary cta-button')
   $('.slider-slide-actions a').removeClass('transparent')
   $('#actividades-dirigidas > div > div.col.col-6.col-last.home-info-section-1-content.in-viewport > a').attr('target', '_BLANK')
